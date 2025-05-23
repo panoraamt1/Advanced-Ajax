9d41353e654deee61e436b5bd9032bf580dad056
panoraamt1 <106094230+panoraamt1@users.noreply.github.com>
2025-05-23 10:13:55 +0300

    4. CountryConroller And view

---
+++ AJAX/AjaxCustomerCRUD/AjaxCustomerCRUD/Controllers/CountryController.cs
@@ -0,0 +1,93 @@
+﻿using AjaxCustomerCRUD.Data;
+using AjaxCustomerCRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AjaxCustomerCRUD.Controllers
+{
+    public class CountryController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CountryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Customer> countries;
+            countries = _context.Countries.ToList();
+            return View(countries);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            Customer country = new Customer();
+            return View(country);
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public IActionResult Create(Customer country)
+        {
+            _context.Add(country);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public IActionResult Details(int Id)
+        {
+            Customer country = GetCountry(Id);
+            return View(country);
+        }
+
+        [HttpGet]
+
+        public IActionResult Edit(int Id)
+        {
+            Customer country = GetCountry(Id);
+            return View(country);
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+
+        public IActionResult Edit(Customer country)
+        {
+            _context.Attach(country);
+            _context.Entry(country).State = EntityState.Modified;
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private Customer GetCountry(int id)
+        {
+            Customer country;
+            country = _context.Countries
+                .Where(c => c.Id == id).FirstOrDefault();
+            return country;
+        }
+
+        [HttpGet]
+
+        public IActionResult Delete(int id)
+        {
+            Customer country = GetCountry(id);
+            return View(country);
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+
+        public IActionResult Delete(Customer country)
+        {
+            _context.Attach(country);
+            _context.Entry(country).State = EntityState.Deleted;
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
+
--- AJAX/AjaxCustomerCRUD/AjaxCustomerCRUD/Program.cs
+++ AJAX/AjaxCustomerCRUD/AjaxCustomerCRUD/Program.cs
@@ -5,7 +5,7 @@ using AjaxCustomerCRUD.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
