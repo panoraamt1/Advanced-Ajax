ef7074dce4001493a3e6d411cd0450701c2d7f81
panoraamt1 <106094230+panoraamt1@users.noreply.github.com>
2025-05-23 10:42:12 +0300

    6. CityController and Views

---
+++ AJAX/AjaxCustomerCRUD/AjaxCustomerCRUD/Controllers/CityController.cs
@@ -0,0 +1,103 @@
+﻿using AjaxCustomerCRUD.Data;
+using AjaxCustomerCRUD.Models;
+using System.Diagnostics.Metrics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace AJAX.Controllers
+{
+    public class CityController : Controller
+    {
+        private readonly AppDbContext _context;
+        public CityController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<City> Cities;
+            Cities = _context.Cities.ToList();
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            City City = new City();
+            ViewBag.Countries = GetCountries();
+            return View(City);
+        }
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public IActionResult Create(City City)
+        {
+            _context.Add(City);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Details(int Id)
+        {
+            City City = _context.Cities.Where(c => c.Id == Id).FirstOrDefault();
+
+            return View(City);
+        }
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            City City = _context.Cities.Where(c =>c.Id == Id).FirstOrDefault();
+
+            ViewBag.Countries = GetCountries();
+            return View(City);
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+
+        public IActionResult Edit(City City)
+        {
+            _context.Attach(City);
+            _context.Entry(City).State = EntityState.Modified;
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public IActionResult Deleat(int Id)
+        {
+            City City = _context.Cities.Where(c => c.Id == Id).FirstOrDefault();
+
+            return View(City);
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+
+        public IActionResult Deleat(City City)
+        {
+            _context.Attach(City);
+            _context.Entry(City).State = EntityState.Modified;
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private List<SelectListItem> GetCountries()
+        {
+            var lstCountries = new List<SelectListItem>();
+            List<Customer> Countries = _context.Countries.ToList();
+            lstCountries = Countries.Select(ct => new SelectListItem()
+            {
+                Value = ct.Id.ToString(),
+                Text = ct.Name
+            }).ToList();
+
+            var defItem = new SelectListItem()
+            {
+                Value = "",
+                Text = "----Select Country----"
+            };
+            lstCountries.Insert(0, defItem);
+            return lstCountries;
+        }
+    }
+}
--- AJAX/AjaxCustomerCRUD/AjaxCustomerCRUD/Program.cs
+++ AJAX/AjaxCustomerCRUD/AjaxCustomerCRUD/Program.cs
@@ -23,6 +23,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Customers}/{action=Index}/{id?}");
+    pattern: "{controller=Country}/{action=Index}/{id?}");
 
 app.Run();
