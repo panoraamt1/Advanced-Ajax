49a8318a188372c509d8f321634b412970f338a6
panoraamt1 <106094230+panoraamt1@users.noreply.github.com>
2025-05-23 12:10:10 +0300

    10. Customer

--- AJAX/AjaxCustomerCRUD/AjaxCustomerCRUD/Data/AppDbContext.cs
+++ AJAX/AjaxCustomerCRUD/AjaxCustomerCRUD/Data/AppDbContext.cs
@@ -16,5 +16,7 @@ namespace AjaxCustomerCRUD.Data
         }
         public virtual DbSet<City> Cities { get; set; }
         public virtual DbSet<Country> Countries { get; set; }
+
+        public virtual DbSet<Customer> Customers { get; set; }
     }
 }
--- AJAX/AjaxCustomerCRUD/AjaxCustomerCRUD/Migrations/20250523090950_AddCustomerTable.Designer.cs
+++ AJAX/AjaxCustomerCRUD/AjaxCustomerCRUD/Migrations/20250523090950_AddCustomerTable.Designer.cs
@@ -3,6 +3,7 @@ using AjaxCustomerCRUD.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AjaxCustomerCRUD.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250523090950_AddCustomerTable")]
+    partial class AddCustomerTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
